dce44c212dad8d3ad05680cd2076da1ed329f114
amndalsr <laisamanda2015@gmail.com>
2024-06-17 21:20:08 -0300

    feat: ajustes no navegador

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ancoras = document.querySelectorAll('.navegador_ancoras a');
+  const sections = document.querySelectorAll('section');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      const navItem = document.querySelector(`.navegador_ancoras a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        ancoras.forEach(a => a.classList.remove('ativa'));
+        navItem.classList.add('ativa');
+      }
+    });
+  }, { threshold: 0.7 });
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
 
   ancoras.forEach(ancora => {
-      ancora.addEventListener('click', function(event) {
-          ancoras.forEach(a => a.classList.remove('ativa'));
-
-          this.classList.add('ativa');
-      });
+    ancora.addEventListener('click', function(event) {
+      ancoras.forEach(a => a.classList.remove('ativa'));
+      this.classList.add('ativa');
+    });
   });
 });
+
 
 function showAlert(message) {
   document.getElementById("alertMessage").innerText = message;
